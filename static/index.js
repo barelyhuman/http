b186b64c604df79714c5e33c864d4c5a6186a1f2
reaper <ahoy@barelyhuman.dev>
2023-02-12 19:56:30 +0530

    feat: add base header for get and head

--- static/index.js
+++ static/index.js
@@ -10,6 +10,18 @@ export class Static {
   }
 
   serve(req, res) {
+    // don't have for requests that aren't GET / HEAD
+    if (req.method !== GET || req.method !== HEAD) {
+      res.status = 405
+      res.end()
+      return
+    }
+
+    // If HEAD, then just end the response without changing status
+    if (req.method == HEAD) {
+      return res.end()
+    }
+
     const reqPath = req.url
     const fpath = join(this.rootPath, reqPath)
 
