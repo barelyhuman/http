014afb3cdf8f6bc81b6db8e2dcd22f3978609ffc
reaper <ahoy@barelyhuman.dev>
2023-02-12 15:45:58 +0530

    remove test code

--- router/index.js
+++ router/index.js
@@ -1,32 +1,3 @@
-import http from 'node:http'
-
-function isDynamicRoute(route) {
-  let routeString = route
-  const dynRegex = /(\[\w+\])/g
-  const matchGroups = route.match(dynRegex) || []
-  matchGroups.forEach(groupItem => {
-    routeString = routeString.replace(groupItem, '((\\w+[-]*)+)')
-  })
-  routeString = routeString.replace(/\//g, '\\/')
-  const parser = RegExp(`^${routeString}$`)
-  return {
-    dynamic: dynRegex.test(route),
-    parse: parser,
-    getParam: url => {
-      const group = url.match(parser).slice(1)
-
-      const params = {}
-
-      matchGroups.forEach((item, index) => {
-        const key = item.replace(/[\[\]]/g, '')
-        params[key] = group[index]
-      })
-
-      return params
-    },
-  }
-}
-
 export class Router {
   routes = {}
   constructor() {
@@ -72,31 +43,29 @@ export class Router {
   }
 }
 
-const router = new Router()
-const obj = { router }
+function isDynamicRoute(route) {
+  let routeString = route
+  const dynRegex = /(\[\w+\])/g
+  const matchGroups = route.match(dynRegex) || []
+  matchGroups.forEach(groupItem => {
+    routeString = routeString.replace(groupItem, '((\\w+[-]*)+)')
+  })
+  routeString = routeString.replace(/\//g, '\\/')
+  const parser = RegExp(`^${routeString}$`)
+  return {
+    dynamic: dynRegex.test(route),
+    parse: parser,
+    getParam: url => {
+      const group = url.match(parser).slice(1)
 
-obj.router.add('get', '/', (req, res) => {
-  res.write('hello')
-  res.end()
-})
+      const params = {}
 
-obj.router.add('post', '/dashboard', (req, res) => {
-  res.write(`from dashboard route`)
-  res.end()
-})
+      matchGroups.forEach((item, index) => {
+        const key = item.replace(/[\[\]]/g, '')
+        params[key] = group[index]
+      })
 
-obj.router.add('get', '/[hello]', (req, res) => {
-  res.write(`yo ${req.params.hello}`)
-  res.end()
-})
-
-const server = http.createServer((req, res) => {
-  const path = req.url
-  const handler = router.find(req.method.toLowerCase(), path)
-  req.params = { ...handler.params }
-  return handler.routeHandler.handler(req, res)
-})
-
-server.listen(3000, () => {
-  console.log('listening')
-})
+      return params
+    },
+  }
+}
