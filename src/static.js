dcb8acaa6f41583c5f877ae4ce3de7d0e27d39ee
reaper <ahoy@barelyhuman.dev>
2023-03-25 21:21:52 +0530

    feat: add urlPrefix and index.html handling
    also fixes a few typos

--- src/static.js
+++ src/static.js
@@ -2,34 +2,57 @@ import { createReadStream, existsSync } from 'fs'
 import { join } from 'path'
 
 export class Static {
-  rootPath
+  rootPath = ''
+  prefix = ''
 
   /**
-   *
+   * @param {string} urlPrefix, the prefix to look for in the url before searching for the file
    * @param {string} rootPath
    */
-  constructor(rootPath) {
+  constructor(urlPrefix, rootPath) {
+    this.prefix = urlPrefix
     this.rootPath = rootPath
     this.serve = this.serve.bind(this)
+    this.hasFile = this.hasFile.bind(this)
+    this._getFilePathFromURL = this._getFilePathFromURL.bind(this)
+  }
+
+  hasFile(req) {
+    const fpath = this._getFilePathFromURL(req)
+    if (!existsSync(fpath)) {
+      return false
+    }
+    return true
+  }
+
+  _getFilePathFromURL(req) {
+    let fpath = req.url.replace(this.prefix, '')
+    if (fpath.trim().length == 0) {
+      fpath = 'index.html'
+    }
+    return join(this.rootPath, fpath)
   }
 
   serve(req, res) {
     // don't have to handle for requests that aren't GET / HEAD
-    if (req.method !== GET || req.method !== HEAD) {
+    if (req.method !== 'GET' && req.method !== 'HEAD') {
       res.status = 405
       res.end()
       return
     }
 
-    // If HEAD, then just end the response without changing status
-    if (req.method == HEAD) {
+    if (!req.url.startsWith(this.prefix)) {
       return res.end()
     }
 
-    const reqPath = req.url
-    const fpath = join(this.rootPath, reqPath)
+    if (req.method == 'HEAD') {
+      // If HEAD, then just end the response without changing status
+      return res.end()
+    }
 
-    if (!existsSync(fpath)) {
+    const fpath = this._getFilePathFromURL(req)
+
+    if (!this.hasFile(req)) {
       res.status = 404
       res.end()
       return
